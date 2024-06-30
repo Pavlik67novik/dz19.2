45a1f869ab8d4bf2a9d8ea90d3e10af18474bba7
Pavel Novikov <pavlik.novik@yandex.ru>
2024-06-30 20:05:13 +0300

    Задание 4

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,6 +10,6 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
         path('', home, name = 'home'),
-        path('contacts', contacts, name = 'contacts')
+        path('/contacts/', contacts, name = 'contacts')
 
 ]
